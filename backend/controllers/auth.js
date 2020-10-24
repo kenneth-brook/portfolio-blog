53d030163bb113621f0b681e365a6cd00c13bb8f
wombat1972 <53923403+wombat1972@users.noreply.github.com>
2020-10-24 13:10:04 -0400

    new user signup set, with DB post

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -1,6 +1,28 @@
+const User = require('../models/user');
+const shortId = require('shortid');
+
 exports.signup = (req, res) => {
-    const {name, email, password} = req.body
-    res.json({
-        user: { name, email, password }
-    });
+    User.findOne({email: req.body.email}).exec((err, user) => {
+        if(user) {
+            return res.status(400).json({
+                error: 'That email is already taken'
+            })
+        }
+
+        const {name, email, password} = req.body
+        let username = shortId.generate()
+        let profile = `${process.env.CLIENT_URL}/profile/${username}`
+
+        let newUser = new User({name, email, password, profile, username})
+        newUser.save((err, success) => {
+            if(err) {
+                return res.status(400).json({
+                    error: err
+                })
+            }
+            res.json({
+                message: 'Signup success! Please signin.'
+            })
+        })
+    })
 };
