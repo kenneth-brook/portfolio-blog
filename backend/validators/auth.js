35b656d827e3346835b4ef4ecb59f71626038d10
wombat1972 <53923403+wombat1972@users.noreply.github.com>
2020-10-24 15:40:34 -0400

    login complete, returning token and setting cookie

--- backend/validators/auth.js
+++ backend/validators/auth.js
@@ -12,3 +12,12 @@ exports.userSignupValidator = [
         .isLength({min: 6})
         .withMessage('Password needs to be at least six caracters in length')
 ];
+
+exports.userSigninValidator = [
+    check('email')
+        .isEmail()
+        .withMessage('Valid email is required'),
+    check('password')
+        .isLength({min: 6})
+        .withMessage('Password needs to be at least six caracters in length')
+];
