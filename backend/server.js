6864b977745a18a2bc704f806e85a425c2a8e1e8
wombat1972 <53923403+wombat1972@users.noreply.github.com>
2020-10-31 17:02:38 -0400

    bug in signup

--- backend/server.js
+++ backend/server.js
@@ -9,28 +9,34 @@ require('dotenv').config();
 const blogRoutes = require('./routes/blog');
 const authRoutes = require('./routes/auth');
 
-const app = express()
+const app = express();
 
 //DataBase
-mongoose.connect(process.env.DATABASE, {useNewUrlParser: true, useCreateIndex: true, useFindAndModify: false, useUnifiedTopology: true }).
-then(() => console.log('DB connected'));
-
-//middleware
-app.use(morgan('dev'))
-app.use(bodyParser.json())
-app.use(cookieParser())
+mongoose
+	.connect(process.env.DATABASE, {
+		useNewUrlParser: true,
+		useCreateIndex: true,
+		useFindAndModify: false,
+		useUnifiedTopology: true,
+	})
+	.then(() => console.log('DB connected'));
 
 //cors
 if (process.env.NODE_ENV == 'development') {
-    app.use(cors({ origin: `${process.env.CLIENT_URL}`}));
+	app.use(cors({ origin: `${process.env.CLIENT_URL}` }));
 }
+
+//middleware
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+app.use(cookieParser());
 
 //routs middleware
 app.use('/api', blogRoutes);
 app.use('/api', authRoutes);
 
 //port
-const port = process.env.PORT || 8000
+const port = process.env.PORT || 8000;
 app.listen(port, () => {
-    console.log(`The beast is running on port ${port}`)
-})
+	console.log(`The beast is running on port ${port}`);
+});
